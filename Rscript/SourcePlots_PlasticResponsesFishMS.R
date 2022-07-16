2ccaf35515049345aceed09983d433433c46b78f
qpetitjean <48917906+qpetitjean@users.noreply.github.com>
2022-07-16 11:47:36 +0200

    Update SourcePlots_PlasticResponsesFishMS.R

--- Rscript/SourcePlots_PlasticResponsesFishMS.R
+++ Rscript/SourcePlots_PlasticResponsesFishMS.R
@@ -1,5 +1,5 @@
 #########################################################################################################################################
-# This is a set of function used to make the differents tables displayed in                                                                    # 
+# This is a set of function used to draw the different plots displayed in                                                                    # 
 # title: "'Adaptive plastic responses to metal contamination in a multistress context: a field experiment in fish'"                     #
 # author:  "Quentin PETITJEAN[q.petitjean1@gmail.com], Pascal LAFFAILLE, Annie PERRAULT, Myriam COUSSEAU, Séverine JEAN, Lisa JACQUIN"  #                                                                                        #
 # date: "25/06/2022"                                                                                                                    #
